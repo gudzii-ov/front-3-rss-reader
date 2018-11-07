84b3960c378940bb320e4b959cab3df2e2706142
Oleg Gduzii <oleg198679@gmail.com>
2018-11-07 22:36:41 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
