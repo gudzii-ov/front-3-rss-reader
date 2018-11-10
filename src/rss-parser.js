360c9da4cb83abf2f61f5c55b195f0910deb1798
Oleg Gduzii <oleg198679@gmail.com>
2018-11-10 20:26:58 +0300

    Fix rss parser

--- src/rss-parser.js
+++ src/rss-parser.js
@@ -6,5 +6,24 @@ export default (feedSource) => {
     throw new Error('Invalid feed source. Try another address');
   }
 
-  console.log(feedDOM);
+  const feedTitle = feedDOM.querySelector('channel title').textContent;
+  const feedDescription = feedDOM.querySelector('channel description').textContent;
+
+  const itemsNodes = feedDOM.querySelectorAll('item');
+  const items = {};
+
+  itemsNodes.forEach((node) => {
+    const nodeTitle = node.querySelector('title').textContent;
+    const nodeDescription = node.querySelector('description').textContent;
+    const nodeLink = node.querySelector('link').textContent;
+    const nodeDate = node.querySelector('pubDate').textContent;
+
+    items[nodeDate] = { nodeTitle, nodeLink, nodeDescription };
+  });
+
+  return {
+    feedTitle,
+    feedDescription,
+    items,
+  };
 };
